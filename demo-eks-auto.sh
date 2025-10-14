b2c491b14466d0d5bd843f8cd084b74ae392d8ad
Christian Melendez <christianhxc@gmail.com>
2025-10-14 16:37:37 +0000

    Updated the EKS Auto Mode demo

--- demo-eks-auto.sh
+++ demo-eks-auto.sh
@@ -10,6 +10,48 @@ source "${SCRIPTPATH}/lib/utils.sh"
 ## - Move to Spot instances
 ## - Split OD & Spot
 
+echo "## - Deploy a dedicated nodepool"
+
+cat << EOF > node-pool-default.yaml
+apiVersion: karpenter.sh/v1
+kind: NodePool
+metadata:
+  name: efficient-compute
+spec:
+  template:
+    metadata:
+      labels:
+        demo: compute-optimization
+    spec:
+      nodeClassRef:
+        group: eks.amazonaws.com
+        kind: NodeClass
+        name: default
+      requirements:
+        - key: karpenter.sh/capacity-type
+          operator: In
+          values: ["on-demand"]
+        - key: eks.amazonaws.com/instance-size
+          operator: NotIn
+          values: [nano, micro, small, medium, large]
+        - key: kubernetes.io/arch
+          operator: In
+          values: ["amd64"]
+        - key: eks.amazonaws.com/instance-generation
+          operator: Gt
+          values: ["2"]
+  limits:
+    cpu: 1000
+    memory: 1000Gi
+  disruption:
+    consolidationPolicy: WhenEmptyOrUnderutilized
+    consolidateAfter: 0s
+    budgets:
+    - nodes: "100%"
+EOF
+
+cmd "kubectl apply -f node-pool-default.yaml"
+
 cmd "echo ..."
 echo "## - Deploy a sample application to see EKS Auto in action"
 
@@ -32,8 +74,7 @@ spec:
         demo: compute-optimization
     spec:
       nodeSelector:
-        kubernetes.io/arch: amd64
-        karpenter.sh/capacity-type: on-demand
+        demo: compute-optimization
       containers:
       - image: public.ecr.aws/eks-distro/kubernetes/pause:3.7
         name: inflate-workload
@@ -45,7 +86,6 @@ spec:
     type: Recreate
 EOF
 
-# cmd "cat deployment-default.yaml"
 cmd "kubectl apply -f deployment-default.yaml"
 cmd "kubectl scale deployment inflate-workload --replicas=10"
 
@@ -56,87 +96,41 @@ cat << EOF > node-pool-default.yaml
 apiVersion: karpenter.sh/v1
 kind: NodePool
 metadata:
-  labels:
-    app.kubernetes.io/managed-by: eks
-  name: general-purpose
+  name: efficient-compute
 spec:
-  disruption:
-    budgets:
-    - nodes: 10%
-    consolidateAfter: 30s
-    consolidationPolicy: WhenEmptyOrUnderutilized
   template:
-    metadata: {}
+    metadata:
+      labels:
+        demo: compute-optimization
     spec:
-      expireAfter: 336h
       nodeClassRef:
         group: eks.amazonaws.com
         kind: NodeClass
         name: default
       requirements:
-      - key: karpenter.sh/capacity-type
-        operator: In
-        values:
-        - on-demand
-      - key: eks.amazonaws.com/instance-category
-        operator: In
-        values:
-        - c
-        - m
-        - r
-      - key: eks.amazonaws.com/instance-generation
-        operator: Gt
-        values:
-        - "4"
-      - key: kubernetes.io/arch
-        operator: In
-        values:
-        - amd64
-        - arm64
-      - key: kubernetes.io/os
-        operator: In
-        values:
-        - linux
-      terminationGracePeriod: 24h0m0s
+        - key: karpenter.sh/capacity-type
+          operator: In
+          values: ["on-demand"]
+        - key: eks.amazonaws.com/instance-size
+          operator: NotIn
+          values: [nano, micro, small, medium, large]
+        - key: kubernetes.io/arch
+          operator: In
+          values: ["amd64","arm64"]
+        - key: eks.amazonaws.com/instance-generation
+          operator: Gt
+          values: ["2"]
+  limits:
+    cpu: 1000
+    memory: 1000Gi
+  disruption:
+    consolidationPolicy: WhenEmptyOrUnderutilized
+    consolidateAfter: 0s
+    budgets:
+    - nodes: "100%"
 EOF
 
 cmd "kubectl apply -f node-pool-default.yaml"
-
-
-cat << EOF > deployment-default.yaml
-apiVersion: apps/v1
-kind: Deployment
-metadata:
-  name: inflate-workload
-  labels:
-    demo: compute-optimization
-spec:
-  selector:
-    matchLabels:
-      app: inflate-workload
-  replicas: 10
-  template:
-    metadata:
-      labels:
-        app: inflate-workload
-        demo: compute-optimization
-    spec:
-      nodeSelector:
-        kubernetes.io/arch: arm64
-        karpenter.sh/capacity-type: on-demand
-      containers:
-      - image: public.ecr.aws/eks-distro/kubernetes/pause:3.7
-        name: inflate-workload
-        resources:
-          requests:
-            cpu: "1"
-            memory: 512M
-  strategy:
-    type: Recreate
-EOF
-
-# cmd "cat deployment-default.yaml"
-cmd "kubectl apply -f deployment-default.yaml"
 
 cmd "echo ..."
 echo "## - Optimize the CPU and Memory requests => will produce underutilized nodes"
@@ -160,8 +154,7 @@ spec:
         demo: compute-optimization
     spec:
       nodeSelector:
-        kubernetes.io/arch: arm64
-        karpenter.sh/capacity-type: on-demand
+        demo: compute-optimization
       containers:
       - image: public.ecr.aws/eks-distro/kubernetes/pause:3.7
         name: inflate-workload
@@ -169,11 +162,8 @@ spec:
           requests:
             cpu: "256m"
             memory: 512Mi
-  strategy:
-    type: Recreate
 EOF
 
-# cmd "cat deployment-default.yaml"
 cmd "kubectl apply -f deployment-default.yaml"
 
 cmd "echo ..."
@@ -183,88 +173,41 @@ cat << EOF > node-pool-default.yaml
 apiVersion: karpenter.sh/v1
 kind: NodePool
 metadata:
-  labels:
-    app.kubernetes.io/managed-by: eks
-  name: general-purpose
+  name: efficient-compute
 spec:
-  disruption:
-    budgets:
-    - nodes: 10%
-    consolidateAfter: 30s
-    consolidationPolicy: WhenEmptyOrUnderutilized
   template:
-    metadata: {}
+    metadata:
+      labels:
+        demo: compute-optimization
     spec:
-      expireAfter: 336h
       nodeClassRef:
         group: eks.amazonaws.com
         kind: NodeClass
         name: default
       requirements:
-      - key: karpenter.sh/capacity-type
-        operator: In
-        values:
-        - on-demand
-        - spot
-      - key: eks.amazonaws.com/instance-category
-        operator: In
-        values:
-        - c
-        - m
-        - r
-      - key: eks.amazonaws.com/instance-generation
-        operator: Gt
-        values:
-        - "4"
-      - key: kubernetes.io/arch
-        operator: In
-        values:
-        - amd64
-        - arm64
-      - key: kubernetes.io/os
-        operator: In
-        values:
-        - linux
-      terminationGracePeriod: 24h0m0s
+        - key: karpenter.sh/capacity-type
+          operator: In
+          values: ["on-demand","spot"]
+        - key: eks.amazonaws.com/instance-size
+          operator: NotIn
+          values: [nano, micro, small, medium, large]
+        - key: kubernetes.io/arch
+          operator: In
+          values: ["amd64","arm64"]
+        - key: eks.amazonaws.com/instance-generation
+          operator: Gt
+          values: ["2"]
+  limits:
+    cpu: 1000
+    memory: 1000Gi
+  disruption:
+    consolidationPolicy: WhenEmptyOrUnderutilized
+    consolidateAfter: 0s
+    budgets:
+    - nodes: "100%"
 EOF
 
-# cmd "cat node-pool-default.yaml"
 cmd "kubectl apply -f node-pool-default.yaml"
-
-cat << EOF > deployment-default.yaml
-apiVersion: apps/v1
-kind: Deployment
-metadata:
-  name: inflate-workload
-  labels:
-    demo: compute-optimization
-spec:
-  selector:
-    matchLabels:
-      app: inflate-workload
-  replicas: 10
-  template:
-    metadata:
-      labels:
-        app: inflate-workload
-        demo: compute-optimization
-    spec:
-      nodeSelector:
-        kubernetes.io/arch: arm64
-        karpenter.sh/capacity-type: spot
-      containers:
-      - image: public.ecr.aws/eks-distro/kubernetes/pause:3.7
-        name: inflate-workload
-        resources:
-          requests:
-            cpu: "256m"
-            memory: 512Mi
-  strategy:
-    type: Recreate
-EOF
-
-# cmd "cat deployment-default.yaml"
-cmd "kubectl apply -f deployment-default.yaml"
 
 cmd "echo ..."
 echo "## - Split OD & Spot"
@@ -273,110 +216,86 @@ cat << EOF > node-pool-default.yaml
 apiVersion: karpenter.sh/v1
 kind: NodePool
 metadata:
-  labels:
-    app.kubernetes.io/managed-by: eks
-  name: general-purpose
+  name: efficient-compute
 spec:
-  disruption:
-    budgets:
-    - nodes: 10%
-    consolidateAfter: 30s
-    consolidationPolicy: WhenEmptyOrUnderutilized
   template:
-    metadata: {}
+    metadata:
+      labels:
+        demo: compute-optimization
     spec:
-      expireAfter: 336h
       nodeClassRef:
         group: eks.amazonaws.com
         kind: NodeClass
         name: default
       requirements:
-      - key: capacity-spread
+        - key: capacity-spread
           operator: In
           values: ["1"]
-      - key: karpenter.sh/capacity-type
-        operator: In
-        values:
-        - on-demand
-      - key: eks.amazonaws.com/instance-category
-        operator: In
-        values:
-        - c
-        - m
-        - r
-      - key: eks.amazonaws.com/instance-generation
-        operator: Gt
-        values:
-        - "4"
-      - key: kubernetes.io/arch
-        operator: In
-        values:
-        - amd64
-        - arm64
-      - key: kubernetes.io/os
-        operator: In
-        values:
-        - linux
-      terminationGracePeriod: 24h0m0s
+        - key: karpenter.sh/capacity-type
+          operator: In
+          values: ["on-demand"]
+        - key: eks.amazonaws.com/instance-size
+          operator: NotIn
+          values: [nano, micro, small, medium, large]
+        - key: kubernetes.io/arch
+          operator: In
+          values: ["amd64","arm64"]
+        - key: eks.amazonaws.com/instance-generation
+          operator: Gt
+          values: ["2"]
+  limits:
+    cpu: 1000
+    memory: 1000Gi
+  disruption:
+    consolidationPolicy: WhenEmptyOrUnderutilized
+    consolidateAfter: 0s
+    budgets:
+    - nodes: "100%"
 EOF
 
 cat << EOF > node-pool-spot.yaml
 apiVersion: karpenter.sh/v1
 kind: NodePool
 metadata:
-  labels:
-    app.kubernetes.io/managed-by: eks
   name: spot
 spec:
-  disruption:
-    budgets:
-    - nodes: 10%
-    consolidateAfter: 30s
-    consolidationPolicy: WhenEmptyOrUnderutilized
   template:
-    metadata: {}
+    metadata:
+      labels:
+        demo: compute-optimization
     spec:
-      expireAfter: 336h
       nodeClassRef:
         group: eks.amazonaws.com
         kind: NodeClass
         name: default
       requirements:
-      - key: capacity-spread
+        - key: capacity-spread
           operator: In
           values: ["2", "3", "4", "5"]
-      - key: capacity-spread
+        - key: karpenter.sh/capacity-type
           operator: In
-          values: ["1"]
-      - key: karpenter.sh/capacity-type
-        operator: In
-        values:
-        - spot
-      - key: eks.amazonaws.com/instance-category
-        operator: In
-        values:
-        - c
-        - m
-        - r
-      - key: eks.amazonaws.com/instance-generation
-        operator: Gt
-        values:
-        - "4"
-      - key: kubernetes.io/arch
-        operator: In
-        values:
-        - amd64
-        - arm64
-      - key: kubernetes.io/os
-        operator: In
-        values:
-        - linux
-      terminationGracePeriod: 24h0m0s
+          values: ["spot"]
+        - key: eks.amazonaws.com/instance-size
+          operator: NotIn
+          values: [nano, micro, small, medium, large]
+        - key: kubernetes.io/arch
+          operator: In
+          values: ["amd64","arm64"]
+        - key: eks.amazonaws.com/instance-generation
+          operator: Gt
+          values: ["2"]
+  limits:
+    cpu: 1000
+    memory: 1000Gi
+  disruption:
+    consolidationPolicy: WhenEmptyOrUnderutilized
+    consolidateAfter: 0s
+    budgets:
+    - nodes: "100%"
 EOF
 
 cmd "kubectl scale deployment inflate-workload --replicas=0"
-# cmd "cat node-pool-default.yaml"
-# cmd "cat node-pool-spot.yaml"
+
 cmd "kubectl apply -f node-pool-default.yaml"
 cmd "kubectl apply -f node-pool-spot.yaml"
 
@@ -398,6 +317,8 @@ spec:
         app: inflate-workload
         demo: compute-optimization
     spec:
+      nodeSelector:
+        demo: compute-optimization
       containers:
       - image: public.ecr.aws/eks-distro/kubernetes/pause:3.7
         name: inflate-workload
@@ -422,60 +343,11 @@ spec:
     type: Recreate
 EOF
 
-# cmd "cat deployment-default.yaml"
 cmd "kubectl apply -f deployment-default.yaml"
 cmd "kubectl scale deployment inflate-workload --replicas=0"
 
 echo "Cleaning up ..."
 kubectl delete deployment inflate-workload > /dev/null 2>&1 || :
-
-cat << EOF > node-pool-default.yaml
-apiVersion: karpenter.sh/v1
-kind: NodePool
-metadata:
-  labels:
-    app.kubernetes.io/managed-by: eks
-  name: general-purpose
-spec:
-  disruption:
-    budgets:
-    - nodes: 10%
-    consolidateAfter: 30s
-    consolidationPolicy: WhenEmptyOrUnderutilized
-  template:
-    metadata: {}
-    spec:
-      expireAfter: 336h
-      nodeClassRef:
-        group: eks.amazonaws.com
-        kind: NodeClass
-        name: default
-      requirements:
-      - key: karpenter.sh/capacity-type
-        operator: In
-        values:
-        - on-demand
-      - key: eks.amazonaws.com/instance-category
-        operator: In
-        values:
-        - c
-        - m
-        - r
-      - key: eks.amazonaws.com/instance-generation
-        operator: Gt
-        values:
-        - "4"
-      - key: kubernetes.io/arch
-        operator: In
-        values:
-        - amd64
-      - key: kubernetes.io/os
-        operator: In
-        values:
-        - linux
-      terminationGracePeriod: 24h0m0s
-EOF
-
-kubectl apply -f node-pool-default.yaml
+kubectl delete --all nodepool > /dev/null 2>&1 || :
 
 rm -rf *.yaml
